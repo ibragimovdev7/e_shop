079c2fb4ef97753371e30fb02cba4b6cd591040c
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2023-06-12 21:19:52 +0500

    add products app and create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    price = models.CharField(max_length=30, null=True)
+    discount_price = models.CharField(max_length=30, null=True)
+    description = models.TextField()
+    color = models.CharField(max_length=15, null=True)
+    size = models.CharField(max_length=5, null=True)
+    count = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
