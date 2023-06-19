4dc6d0a7432e0fec05735bf6b3f2252834627ddc
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2023-06-19 20:02:00 +0500

    default database sqlite3 add for prod

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -81,13 +81,17 @@ WSGI_APPLICATION = 'e_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'e_shop_db',
-        'USER': 'e_shop_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'e_shop_db',
+    #     'USER': 'e_shop_user',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 # Password validation
